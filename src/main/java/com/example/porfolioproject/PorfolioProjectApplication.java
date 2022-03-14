4b046a5d7a91eb5d788634d5095d1804eee7c414
chris <chri402s@stud.kea.dk>
2022-03-14 09:51:19 +0100

    Initial commit

---
+++ src/main/java/com/example/porfolioproject/PorfolioProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.porfolioproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PorfolioProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PorfolioProjectApplication.class, args);
+    }
+
+}
